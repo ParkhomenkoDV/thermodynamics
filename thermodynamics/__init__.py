7fffcc782067daafcacc0e523fc1c3b15bdafeb6
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-07-30 00:24:16 +0300

    fix __init__

--- thermodynamics/__init__.py
+++ thermodynamics/__init__.py
@@ -1,4 +1,2 @@
 from .parameters import parameters
-
-from .thermodynamics import T0, GAS_CONST
-from .thermodynamics import gdf, atmosphere_standard, efficiency_polytropic
+from .thermodynamics import *
