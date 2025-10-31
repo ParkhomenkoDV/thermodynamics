80d567fa836b731820ecb3e7ac892dcba3d2cb37
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-10-31 14:23:14 +0300

    add heat_capacity

--- thermodynamics/parameters.py
+++ thermodynamics/parameters.py
@@ -2,11 +2,14 @@ from mathematics import Constants
 
 # термодинамические параметры
 parameters = Constants(
-    m="mass",
-    t="temperature",
-    p="pressure",
-    d="density",
-    mf="mass_flow",
+    m="mass",  # масса
+    t="temperature",  # температура
+    p="pressure",  # далвение
+    d="density",  # плотность
+    mf="mass_flow",  # массовый расход
     eo="excess_oxidizing",  # коэффициент избытка окислителя
-    gc="gas_const",
+    gc="gas_const",  # газовая постоянная
+    hc="heat_capacity",  # теплоемкость
+    hcp="heat_capacity_pressure",  # теплокмкость при постоянном давлении
+    hcv="heat_capacity_volume",  # теплокмкость при постоянном объеме
 )
